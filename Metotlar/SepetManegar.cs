459cea282750c07c784a4c59747f578a24ae34ac
Giz943 <77889952+Giz943@users.noreply.github.com>
2021-01-31 18:18:55 +0300

    Yeni düzenleme yapıldı.

---
+++ Metotlar/SepetManegar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Metotlar
+{
+    public class SepetManegar
+    {
+        public void Ekle(Urunn urun)
+        {
+            Console.WriteLine("Tebrikler.Sepete eklendi : " + urun.Adi);
+
+        }
+
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+
+            Console.WriteLine("Tebrikler.Sepete eklendi : " + urunAdi);
+
+        }
+
+
+
+
+
+    }
+}
